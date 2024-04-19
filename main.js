628a98b241101b65881de7e56f5db7f2dea26fda
emilykaroliny2006 <129808025+emilykaroliny2006@users.noreply.github.com>
2024-04-18 22:29:01 -0300

    jo

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 const numeroSenha = document.querySelector('.parametro-senha__textoi');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-const letraMaiusculas = 
+const letraMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letraMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelectorAll('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('forca');
+
+botoes [0].onclick = diminuiTamanho;
+botoes [1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        //tamanhoSenh = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    
+
+
