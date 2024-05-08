f196ece08f6d71a81375980fb09201f7e87bd646
emilykaroliny2006 <129808025+emilykaroliny2006@users.noreply.github.com>
2024-05-08 19:27:57 -0300

    ert

--- main.js
+++ main.js
@@ -76,5 +76,3 @@ function diminuiTamanho() {
         valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha. ";
     }
     
-
-
